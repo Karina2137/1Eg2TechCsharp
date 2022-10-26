047e32225d915483cd3b99edee1dd3335b3c055a
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-26 09:35:07 +0200

    Update KartaPracy3a.cs

--- KartaPracy3a.cs
+++ KartaPracy3a.cs
@@ -1,55 +1,141 @@
 //Zad.1
-//int n = int.Parse(Console.ReadLine());
-//for (int i = 0; i < n; i++)
-{
-//    Console.Write(Console.ReadLine());  
-}
-
-//Zad.2
-//Tabliczka mnożenia
-
-//for (int i = 1; i < 10; i++)
-{
-  //  for (int j = 1; j < 10; j++)
+ //int n = int.Parse(Console.ReadLine());
+ //for (int i = 0; i < n; i++)
     {
-  //      Console.Write(i*j + "\t");
+        //    Console.Write(Console.ReadLine());  
     }
-  //  Console.WriteLine();
-}
 
-//Zad.3
-//int n = int.Parse(Console.ReadLine());
-//for (int i = 0; i < n; i++)
-{
-//    for (int j = 0; j < i; j++)
-    {
-//        Console.Write("*");
-    }
-//   Console.WriteLine();
-}
-//Console.WriteLine(); 
-//Console.WriteLine();
+    //Zad.2
+    //Tabliczka mnożenia
 
-//for (int i = 0;i < n; i++)
-{
-//    for(int j = 0; j < n - i; j++)
+    //for (int i = 1; i < 10; i++)
     {
-//        Console.Write("*");
+        //  for (int j = 1; j < 10; j++)
+        {
+            //      Console.Write(i*j + "\t");
+        }
+        //  Console.WriteLine();
     }
+
+    //Zad.3
+ // int n = int.Parse(Console.ReadLine());
+ //for (int i = 0; i < n; i++)
+    {
+   //       for (int j = 0; j < i; j++)
+        {
+   //               Console.Write("*");
+        }
+   //      Console.WriteLine();
+    }
+  //Console.WriteLine(); 
+  //Console.WriteLine();
+
+ // for (int i = 0;i < n; i++)
+    {
+  //        for(int j = 0; j < n - i; j++)
+        {
+  //                Console.Write("*");
+       }
+   //      Console.WriteLine();
+   }
+   //onsole.WriteLine();
+  //Console.WriteLine();
+
+  //for (int i = 0; i < n; i++)
+    {
+   //     for (int j = 0; j < n - i-1; j++)
+        {
+    //  Console.Write(" ");
+        }
+    //    for (int k = n - i -1; k < n ; k++)
+        {
+     //             Console.Write("*");
+        }
+     //    Console.WriteLine();
+    }
+  //Console.WriteLine();
+   //onsole.WriteLine();
+
+   //or (int i = 0; i < n; i++)
+    {
+   //   for (int j = 0; j < i + 1j++)
+        {
+   //       Console.Write("-");
+        }
+    //  for (int k = i; k < n; k++)
+        {
+
+        }
+
+//      Console.WriteLine();
+    }
+    //1
+    //nt n = int.Parse(Console.ReadLine());
+    //for (int i = 1; i < n+1; i++)
+    //{
+    //    for (int j = 0; j < i+1; j++)
+    //        if (1 < j) Console.Write("*");
+    //            Console.Write("*");
+    //        else  Console.Write(" ");
+
+
+
+}
+//2
+//for (int i = 1; i < n; i++)
+//    {
+//        for (int j = 1; j < i + 1; j++)
+//            if (n-j + 1 >= j) Console.Write("*");
+//            else Console.Write(" ");
+//        Console.WriteLine();
+//    }
+////3
+//for (int i = 1; i < n + 1; i++)
+//{
+//    for (int j = 1; j < i + 1; j++)
+//        if (n - j + 1 < i) Console.Write("*");
+//        else Console.Write(" ");
 //    Console.WriteLine();
-}
-//Console.WriteLine();
-//Console.WriteLine();
+//}
 
-//for (int i = 0; i < n; i++)
-{
-//    for (int j = 0; j < n - i-1; j++)
-    {
-        Console.Write(" ");
-    }
-//    for (int k = n - i -1; k < n ; k++)
-    {
-//        Console.Write("*");
-    }
-//    Console.WriteLine();
-}
+////4
+//for (int i = 1; i < n + 1; i++)
+//    {
+//        for (int j = 1; j < i + 1; j++)
+//            if (i >= j) Console.Write("*");
+//            else Console.Write(" ");
+//        Console.WriteLine();
+//    }
+
+//Zad.7
+//int n = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i < n + 1; i++)
+//{
+//    for (int j = 0; j < n + 1; j++)
+//    {
+//        if (i == 1 || j == 1 || j == n || i == n || (i == n / 2 + 1 || j == n / 2 + 1) Console.Write(" ");
+//        else Console.Write(" ");
+//    }
+//}
+
+//Zad.5
+//int n = int.Parse(Console.ReadLine());
+//for (int i = 1; i < n+1; i++)
+//{
+//    for (int j = 1; j < n+1; j++)
+//    {
+//    if(j==n/2+1)
+//        {
+//            Console.WriteLine("*");
+//        }
+//    else if (i == n / 2 + 1)
+//        {
+//            Console.WriteLine("?");
+//        }
+//        else
+//        {
+//            Console.WriteLine(" ");
+//        }
+//    }
+//}
