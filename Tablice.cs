c246f36c869e90c0126c76a18a7470d898c3bdb6
25kp <114558445+Karina2137@users.noreply.github.com>
2022-11-23 09:22:05 +0100

    Create Tablice.cs

---
+++ Tablice.cs
@@ -0,0 +1,57 @@
+// Tablice - zmnienna (kolekcja) do trzymania wielu zmiennych
+/*
+int a = 3;
+int b = 5;
+int c = 6;
+int d = 7;
+
+int e ;
+
+int[] T = new int[4];
+
+T[0] = 3;
+T[1] = 5;
+T[2] = 6;
+T[3] = 7;
+T[4] = 8;
+
+Console.WriteLine(T[3] + " " + T[2]);
+
+for (int i = 0; i <T.Length; i++)
+{
+    Console.WriteLine(T[i]);
+}
+
+int[] K = new int[] { 2, 3, 5, 8, 13, 21, 34, 55 };
+for (int i = 0; i < K.Length; i++)
+{
+    Console.Write($"{K[i]}  ");         // $ możliwość urzywania zmiennych w klamrach { } w cudzysłowiu
+    //Console.Write({K[i]} + " ");
+}
+
+//Znajdź maksymalną wartość tablicy [4,5,8,9,7,6]
+
+int[] L = new int[] { 4, 5, 8, 9, 7, 6 };
+int maks = L[0];
+
+for (int i = 0; i < L.Length; i++)
+{
+    if (L[i] > maks)
+    {
+        maks = L[i];
+    }
+}
+Console.Write(maks);
+
+//Znajdź minimalną wartość tablicy [6,2,1,4,9,5]
+int[] M = new int[] { 6, 2, 1, 4, 9, 5 };
+int minimum = M[0];
+for (int i = 0; i < M.Length; i++)
+{
+    if (M[i] < minimum)
+    {
+        minimum = M[i];
+    }
+}
+Console.Write(minimum);
+*/
